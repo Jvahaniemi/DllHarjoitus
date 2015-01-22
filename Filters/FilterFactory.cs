cf8485b578bcc01d832d6541b8a53ecca0b6886c
j <none>
2015-01-22 18:44:55 +0200

    eka versio

---
+++ Filters/FilterFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filters
+{
+    public enum FilterTypes
+    {
+        Median, Average
+    }
+
+    public class FilterFactory
+    {       
+        public static Filter buildFilter(FilterTypes type, int lenght)
+        {
+            Filter filter = null;
+            
+            switch (type)
+            {
+                case FilterTypes.Average:
+                    filter = new AverageFilter(lenght);
+                    break;
+                case FilterTypes.Median:
+                    filter = new MedianFilter(lenght);
+                    break;
+                default:
+                    break;
+            }
+            return filter;
+        }
+    }
+}
